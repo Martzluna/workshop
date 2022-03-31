920311b91575a60d79f207ecaee8c4395318770b
Jarling Duque <jarlingduqueduarte@gmail.com>
2022-03-31 17:22:26 -0500

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import logo from '../images/nvidia-logo.PNG'
+import gtc from '../images/gtc.png'
+import mundo from '../images/logo-mundo.png'
+import sign from '../images/sign-in.png'
+
+const tab = <>&nbsp;&nbsp;&nbsp;&nbsp;</>;
 
 function Navbar() {
   return (
     <div>
+      {/* nav 1 */}
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
@@ -12,9 +18,10 @@ function Navbar() {
           <ul class="navbar-nav">
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                EN
+              <img src={mundo} alt="" width="auto" height="auto" class="d-inline-block align-text-top" />
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                
                 <li><a class="dropdown-item" href="#">ES</a></li>
                 <li><a class="dropdown-item" href="#">EN</a></li>
                 <li><a class="dropdown-item" href="#">PT</a></li>
@@ -23,32 +30,38 @@ function Navbar() {
           </ul>
         </div>
       </nav>
+      {/* nav 2 */}
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            <img src={logo} alt="" width="auto" height="auto" class="d-inline-block align-text-top" />
+            <img src={gtc} alt="" width="auto" height="auto" class="d-inline-block align-text-top" />
+            {tab} CONFERENCE & TRAINING 
+            {tab} MARCH 31 - 24, 2022
+            {tab}KEYNOTE MARCH 22
           </a>
           <ul class="navbar-nav">
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                EN
+              <a> 
+              <img src={sign} alt="" width="auto" height="auto" class="d-inline-block align-text-top" />
+              </a>
               </a>
             </li>
           </ul>
         </div>
       </nav>
+      {/* nav 3 */}
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            <img src={logo} alt="" width="auto" height="auto" class="d-inline-block align-text-top" />
+          Keynote  {tab} 
+          <a>Session Catalog</a>{tab} 
+          <a> For You</a>{tab} 
+          <a>Workshops & Training</a>{tab} 
+          <a>Special Programs</a>{tab} 
+          <a>Sponsors</a>{tab} 
+          <a>More</a>
           </a>
-          <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                EN
-              </a>
-            </li>
-          </ul>
         </div>
       </nav>
     </div>
